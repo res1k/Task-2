91d97356cac3a15bd5a599980f3a5a7b78b56de7
Andrey Zhivotov <res_a@mail.ru>
2022-02-22 15:10:18 +0300

    Initial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Andrey Zhivotov on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
